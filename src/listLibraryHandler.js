0a6cc8e8ff621a01c93485a70ead7a4e1653353f
Behindtheboard <jk2624@gmail.com>
2024-07-08 13:02:52 -0700

    Import list and task js to handler for button functionality

--- src/listLibraryHandler.js
+++ src/listLibraryHandler.js
@@ -1,7 +1,11 @@
+import displayList from "./displayList";
+import displayTask from "./displayTask";
+
 export default function listLibraryHandler(obj) {
     obj.library.forEach((list) =>{
         document.querySelector(`#${list.name.replace(/\s+/g, '')}-list-button`).addEventListener('click', () => {
-            console.log(`${list.name.replace(/\s+/g, '')}-list-button`);
+            displayList(list);
+            displayTask(list);
         });
     });
 }
